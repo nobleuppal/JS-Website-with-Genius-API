0628e5cf2a8de68639378ee0693899a292bfe669
Noble Uppal <noble.uppal@outlook.com>
2022-03-12 15:40:10 -0500

    adding modal with song details

--- genius.js
+++ genius.js
@@ -6,21 +6,93 @@ const eminem = fetch("https://genius.p.rapidapi.com/artists/45/songs?sort=popula
 	}
 })
 
-
-
+const modalOpen = '[data-open]';
+const modalClose = '[data-close]';
+const isVisible = 'is-visible';
+const emSongs = [];
+let i = 0;
+const openModal = document.querySelectorAll(modalOpen);
+const closeModal = document.querySelectorAll(modalClose);
 
 eminem
 .then(data => data.json())
-.then(data => { data.response.songs.forEach(song => {
+.then(data => { data.response.songs.forEach((song) => {
        let div = document.createElement('div');
        document.getElementById('all-songs').appendChild(div);
        div.innerHTML += song.full_title;
        div.classList.add('top-thirty');
-}); })
+       div.setAttribute('id', i);
+       div.setAttribute('data-open', 'songs-details');
+
+       let img = document.createElement('img');
+       img.src = '/assets/Elogo.png';
+       div.appendChild(img);
+
+       emSongs.push(song);
+       i++;
+
+       });
+ })
 .catch(err => console.log(err));
 
-//console.log(eminem.response.songs[0].full_title);
-// eminem.response.songs.foreach(function (song) {
-//       
-// });
+setTimeout(function () {
+       const singles = document.getElementsByClassName('top-thirty');
+       
+       for (const elm of singles) {
+              elm.addEventListener('click', function() {
+                     const modalID = this.dataset.open;
+                     document.getElementById(modalID).classList.add(isVisible);  
+                     
+                     console.log(emSongs[elm.id].full_title); 
+
+              
+                     let div = document.createElement('div');
+                     document.getElementById('song-info').appendChild(div);
+                     div.innerHTML += emSongs[elm.id].full_title;
+
+                     let img = document.createElement('img');
+                     document.getElementById('song-info').appendChild(img);
+                     console.log(emSongs);
+                     img.src = emSongs[elm.id].song_art_image_url;
+
+                     let a = document.createElement('a');
+                     document.getElementById('song-info').appendChild(a);
+                     a.href = emSongs[elm.id].url;
+                     a.innerHTML += 'See Lyrics';
+                                 
+              });  
+       }
+}, 1500);
+
+
+
+setTimeout(function () {
+       for (const elm of closeModal) {
+              elm.addEventListener('click', function() {
+                     this.parentElement.classList.remove(isVisible);
+                     let element = document.getElementById('song-info');
+                     while(element.firstChild) {
+                            element.removeChild(element.firstChild);
+                     }
+              });  
+       }
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
